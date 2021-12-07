5a71e0a8b3cc14162ca1ba3c8f08f5d3754d8274
gerald.schuster <gerald.schuster@pm.me>
2021-12-07 06:41:12 +0100

    day07

---
+++ Day07/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+var input = File.ReadAllText("input.txt").Split(",").Select(long.Parse).ToList();
+
+var min = (int)input.Min();
+var max = (int)input.Max();
+
+var minSum = long.MaxValue;
+for (var i = min; i <= max; i++) {
+  var sum = input.Select(x => Math.Abs(i - x)).Sum();
+  if (sum < minSum) minSum = sum;
+}
+
+Console.Out.WriteLine(minSum);
+
+minSum = long.MaxValue;
+for (var i = min; i <= max; i++) {
+  var sum = input.Select(x => Enumerable.Range(1, (int)Math.Abs(i - x)).Sum()).Sum();
+  if (sum < minSum) minSum = sum;
+}
+
+Console.Out.WriteLine(minSum);
