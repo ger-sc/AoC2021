52679ed4c8b2da1c247d1b68772f6e8519b0a2d5
gerald.schuster <gerald.schuster@pm.me>
2021-12-15 18:51:41 +0100

    performance

--- Day15/Program.cs
+++ Day15/Program.cs
@@ -3,6 +3,7 @@ using System.Drawing;
 
 var input = File.ReadAllLines("input.txt").ToList();
 
+
 var inputMap = new Dictionary<Point, long>();
 for (var y = 0; y < input.Count; y++) {
   for (var x = 0; x < input[y].Length; x++) {
@@ -11,7 +12,7 @@ for (var y = 0; y < input.Count; y++) {
 }
 
 var sw = Stopwatch.StartNew();
-var result = AStar(inputMap);
+var result = AStar(inputMap, input[0].Length, input.Count());
 sw.Stop();
 Console.Out.WriteLine($"{result} in {sw.Elapsed.TotalSeconds} s");
 
@@ -19,8 +20,8 @@ var maxX = input.First().Length;
 var maxY = input.Count;
 
 var inputMap2 = new Dictionary<Point, long>();
-for (var y = 0; y < input.Count * 5; y++) {
-  for (var x = 0; x < input.First().Length * 5; x++) {
+for (var y = 0; y < maxY * 5; y++) {
+  for (var x = 0; x < maxX * 5; x++) {
     var origY = y % maxY;
     var origX = x % maxX;
     var add = x / maxX + y / maxY;
@@ -35,13 +36,13 @@ for (var y = 0; y < input.Count * 5; y++) {
   }
 }
 sw = Stopwatch.StartNew();
-var result2 = AStar(inputMap2);
+var result2 = AStar(inputMap2, input.First().Length * 5, input.Count * 5);
 sw.Stop();
 Console.Out.WriteLine($"{result2} in {sw.Elapsed.TotalSeconds} s");
 
-long AStar(Dictionary<Point, long> map) {
+long AStar(Dictionary<Point, long> map, int width, int height) {
   var openList = new Dictionary<Point, long>();
-  var closedList = new List<Point>();
+  var closedList = new Dictionary<Point, long>();
 
   var goal = new Point(map.Keys.Max(k => k.X), map.Keys.Max(k => k.Y));
 
@@ -55,28 +56,26 @@ long AStar(Dictionary<Point, long> map) {
       return currentNode.Value;
     }
 
-    closedList.Add(currentNode.Key);
-    ExpandNode(currentNode, openList, closedList, map);
+    closedList.Add(currentNode.Key, 0);
+    ExpandNode(currentNode, openList, closedList, map, width, height);
 
   } while (openList.Any());
 
   return 0;
 }
 
-void ExpandNode(KeyValuePair<Point, long> node, Dictionary<Point, long> openList, List<Point> closedList, Dictionary<Point, long> map) {
-  foreach (var successor in GetNeighbors(node.Key, map)) {
-    if(closedList.Contains(successor)) continue;
-    var costs = node.Value + map[successor];
+void ExpandNode(KeyValuePair<Point, long> node, IDictionary<Point, long> openList, IReadOnlyDictionary<Point, long> closedList, Dictionary<Point, long> map, int width, int height) {
+  var (key, value) = node;
+  foreach (var successor in GetNeighbors(key, map, width, height).Where(n => !closedList.ContainsKey(n))) {
+    var costs = value + map[successor];
     if(openList.ContainsKey(successor) && costs >= openList[successor]) continue;
     openList[successor] = costs;
   }
 }
 
-IEnumerable<Point> GetNeighbors(Point node, Dictionary<Point, long> map) {
-  var maxPointX = map.Keys.Max(k => k.X);
-  var maxPointY = map.Keys.Max(k => k.Y);
+IEnumerable<Point> GetNeighbors(Point node, Dictionary<Point, long> map, int width, int height) {
   if (node.X > 0) yield return new Point(node.X - 1, node.Y);
-  if (node.X < maxPointX) yield return new Point(node.X + 1, node.Y);
+  if (node.X < width - 1) yield return new Point(node.X + 1, node.Y);
   if (node.Y > 0) yield return new Point(node.X, node.Y - 1);
-  if (node.Y < maxPointY) yield return new Point(node.X, node.Y + 1);
+  if (node.Y < height - 1) yield return new Point(node.X, node.Y + 1);
 }
