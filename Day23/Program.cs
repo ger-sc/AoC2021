ee0e0c19fa463be882581068b4d57aefea7d4ddb
gerald.schuster <gerald.schuster@pm.me>
2022-01-05 23:31:04 +0100

    day23 refactoring

--- Day23/Program.cs
+++ Day23/Program.cs
@@ -1,135 +1,105 @@
 ﻿using System.Drawing;
 
-/*var dict = new Dictionary<Point, AmphiPodType> {
-  { new Point(2, 1), AmphiPodType.B },
-  { new Point(2, 2), AmphiPodType.A },
-  { new Point(4, 1), AmphiPodType.C },
-  { new Point(4, 2), AmphiPodType.D },
-  { new Point(6, 1), AmphiPodType.B },
-  { new Point(6, 2), AmphiPodType.C },
-  { new Point(8, 1), AmphiPodType.D },
-  { new Point(8, 2), AmphiPodType.A }
-};*/
-
-var dict = new Dictionary<Point, AmphiPodType> {
-  { new Point(2, 1), AmphiPodType.D },
-  { new Point(2, 2), AmphiPodType.C },
-  { new Point(4, 1), AmphiPodType.B },
-  { new Point(4, 2), AmphiPodType.C },
-  { new Point(6, 1), AmphiPodType.D },
-  { new Point(6, 2), AmphiPodType.A },
-  { new Point(8, 1), AmphiPodType.A },
-  { new Point(8, 2), AmphiPodType.B }
+var dict = new Dictionary<Point, PodType> {
+  { new Point(2, 1), PodType.D },
+  { new Point(2, 2), PodType.C },
+  { new Point(4, 1), PodType.B },
+  { new Point(4, 2), PodType.C },
+  { new Point(6, 1), PodType.D },
+  { new Point(6, 2), PodType.A },
+  { new Point(8, 1), PodType.A },
+  { new Point(8, 2), PodType.B }
 };
 
 var board = new Board(dict, 2);
 
-var results = new HashSet<int>();
+var results = new SortedSet<int> { int.MaxValue };
 var history = new Dictionary<string, int>();
-//Step(board, 0, 0, ref results, ref history);
 
-//Console.Out.WriteLine(results.Min());
+Step(board, 0, new Point(0, 0), GetPoints(2), ref results, ref history);
 
-var dict2 = new Dictionary<Point, AmphiPodType> {
-  { new Point(2, 1), AmphiPodType.D },
-  { new Point(2, 2), AmphiPodType.D },
-  { new Point(2, 3), AmphiPodType.D },
-  { new Point(2, 4), AmphiPodType.C },
-  { new Point(4, 1), AmphiPodType.B },
-  { new Point(4, 2), AmphiPodType.C },
-  { new Point(4, 3), AmphiPodType.B },
-  { new Point(4, 4), AmphiPodType.C },
-  { new Point(6, 1), AmphiPodType.D },
-  { new Point(6, 2), AmphiPodType.B },
-  { new Point(6, 3), AmphiPodType.A },
-  { new Point(6, 4), AmphiPodType.A },
-  { new Point(8, 1), AmphiPodType.A },
-  { new Point(8, 2), AmphiPodType.A },
-  { new Point(8, 3), AmphiPodType.C },
-  { new Point(8, 4), AmphiPodType.B }
+Console.Out.WriteLine("Part1: "+results.Min());
+
+var dict2 = new Dictionary<Point, PodType> {
+  { new Point(2, 1), PodType.D },
+  { new Point(2, 2), PodType.D },
+  { new Point(2, 3), PodType.D },
+  { new Point(2, 4), PodType.C },
+  { new Point(4, 1), PodType.B },
+  { new Point(4, 2), PodType.C },
+  { new Point(4, 3), PodType.B },
+  { new Point(4, 4), PodType.C },
+  { new Point(6, 1), PodType.D },
+  { new Point(6, 2), PodType.B },
+  { new Point(6, 3), PodType.A },
+  { new Point(6, 4), PodType.A },
+  { new Point(8, 1), PodType.A },
+  { new Point(8, 2), PodType.A },
+  { new Point(8, 3), PodType.C },
+  { new Point(8, 4), PodType.B }
 };
 
 var board2 = new Board(dict2, 4);
 
-var results2 = new HashSet<int>();
+var results2 = new SortedSet<int> { int.MaxValue };
 var history2 = new Dictionary<string, int>();
-Step(board2, 0, 0, ref results2, ref history2);
+Step(board2, 0, new Point(0, 0), GetPoints(4), ref results2, ref history2);
 
-Console.Out.WriteLine(results2.Min());
+Console.Out.WriteLine("Part2: "+results2.Min());
 
-void Step(Board b, int energy, int level, ref HashSet<int> solutions, ref Dictionary<string, int> hist) {
-  var boardString = b.ToString();
-
-  if (energy > 42106) return;
-  
-  if (hist.ContainsKey(boardString)) {
-    if (hist[boardString] < energy) {
-      return;
-    }
-    hist[boardString] = energy;
-  }
-  else {
-    hist[boardString] = energy;
-  }
-  
-  if (level == 1) {
-    PrintBoard(b);
-    Console.Out.WriteLine();
-  }
-
-  if (solutions.Any() && solutions.Min() < energy) return;
-  
+void Step(Board b, int energy, Point lastMove, IList<Point> pointList, ref SortedSet<int> solutions,
+  ref Dictionary<string, int> hist) {
   if (b.IsFinished()) {
-    if (!solutions.Any() || energy < solutions.Min()) {
-      Console.Out.WriteLine(energy);
-    }
     solutions.Add(energy);
     return;
   }
 
-  foreach (var pod in b.Amphipods.Where(p => !IsInEndPosition(b, p.Key, p.Value) && CanMove(b, p.Key))) {
-    if (IsRoom(pod.Key)) {
-      var availableNewPoints = b.AvailablePoints().Where(p => !IsExit(p) && (IsCorrectRoom(p, pod.Value) || IsCorridor(p)));
-      foreach (var newPoint in availableNewPoints.Where(np => CanMoveTo(b, pod.Key, np))) {
-        var (newBoard, newEnergy) = b.Move(pod.Key, newPoint);
-        Step(newBoard, energy + newEnergy, level + 1, ref solutions, ref hist);
+  var boardHash = b.ToString();
+  if (hist.ContainsKey(boardHash)) {
+    if (hist[boardHash] < energy) {
+      return;
+    }
+    hist[boardHash] = energy;
+  }
+  else {
+    hist[boardHash] = energy;
+  }
+
+  if (solutions.Any() && solutions.Min() < energy) return;
+
+  foreach (var (key, value) in b.Pods.Where(p => !IsInEndPosition(b, p.Key, p.Value) && CanMove(b, p.Key) && p.Key != lastMove)) {
+    if (IsRoom(key)) {
+      var availableNewPoints = pointList.Except(b.Pods.Keys).Where(p =>
+        !IsExit(p) && (IsCorrectRoom(p, value) && b.IsRoomAvailable(value) || IsCorridor(p)));
+      foreach (var newPoint in availableNewPoints.Where(np => CanMoveTo(b, key, np))) {
+        var (newBoard, newEnergy) = b.Move(key, newPoint);
+        Step(newBoard, energy + newEnergy, newPoint, pointList, ref solutions, ref hist);
       }
     }
     else {
-      var availableNewPoint = b.AvailablePoints().Where(p => IsCorrectRoom(p, pod.Value)).ToList();
+      var availableNewPoint = pointList.Except(b.Pods.Keys).Where(p => IsCorrectRoom(p, value)).ToList();
       if (availableNewPoint.Any()) {
         var newPoint = availableNewPoint.Single(x => x.Y == availableNewPoint.Max(p => p.Y));
-        if (!CanMoveTo(b, pod.Key, newPoint) || !b.IsRoomAvailable(pod.Value)) continue;
-        var (newBoard, newEnergy) = b.Move(pod.Key, newPoint);
-        Step(newBoard, energy + newEnergy, level + 1, ref solutions, ref hist);
+        if (!CanMoveTo(b, key, newPoint) || !b.IsRoomAvailable(value)) continue;
+        var (newBoard, newEnergy) = b.Move(key, newPoint);
+        Step(newBoard, energy + newEnergy, newPoint, pointList, ref solutions, ref hist);
       }
     }
   }
+}
+
+IList<Point> GetPoints(int roomSize) {
+  return Enumerable.Range(0, 11)
+    .Select(x => new Point(x, 0))
+    .ToList()
+    .Concat(Enumerable.Range(1, 4)
+      .SelectMany(x => Enumerable.Range(1, roomSize)
+        .Select(index => new Point(2 * x, index))))
+    .ToList();
 }
 
 bool CanMove(Board b, Point p) {
-  return IsCorridor(p) || b.Amphipods.Where(a => a.Key.X == p.X).Min(a => a.Key.Y) == p.Y;
-}
-
-void PrintBoard(Board b) {
-  for (var y = 0; y <= b.RoomSize; y++) {
-    for (var x = 0; x <= 10; x++) {
-      var point = new Point(x, y);
-      if (b.Points.Contains(point)) {
-        if (b.Amphipods.ContainsKey(point)) {
-          Console.Out.Write(b.Amphipods[point]);
-        }
-        else {
-          Console.Out.Write(".");
-        }
-      }
-      else {
-        Console.Out.Write(" ");
-      }
-    }
-
-    Console.Out.WriteLine();
-  }
+  return IsCorridor(p) || b.Pods.Where(a => a.Key.X == p.X).Min(a => a.Key.Y) == p.Y;
 }
 
 bool CanMoveTo(Board b, Point start, Point end) {
@@ -153,25 +123,25 @@ bool CanMoveTo(Board b, Point start, Point end) {
     moves.Add(new Point(x, y));
   }
 
-  return moves.All(p => !b.Amphipods.ContainsKey(p));
+  return moves.All(p => !b.Pods.ContainsKey(p));
 }
 
-bool IsInEndPosition(Board b, Point p, AmphiPodType type) {
+bool IsInEndPosition(Board b, Point p, PodType type) {
   return type switch {
-    AmphiPodType.A => IsRoomA(p) && b.Amphipods.Where(x => IsRoomA(x.Key) && x.Key.Y > p.Y).All(x => x.Value == AmphiPodType.A),
-    AmphiPodType.B => IsRoomB(p) && b.Amphipods.Where(x => IsRoomB(x.Key) && x.Key.Y > p.Y).All(x => x.Value == AmphiPodType.B),
-    AmphiPodType.C => IsRoomC(p) && b.Amphipods.Where(x => IsRoomC(x.Key) && x.Key.Y > p.Y).All(x => x.Value == AmphiPodType.C),
-    AmphiPodType.D => IsRoomD(p) && b.Amphipods.Where(x => IsRoomD(x.Key) && x.Key.Y > p.Y).All(x => x.Value == AmphiPodType.D),
+    PodType.A => IsRoomA(p) && b.Pods.Where(x => IsRoomA(x.Key) && x.Key.Y > p.Y).All(x => x.Value == PodType.A),
+    PodType.B => IsRoomB(p) && b.Pods.Where(x => IsRoomB(x.Key) && x.Key.Y > p.Y).All(x => x.Value == PodType.B),
+    PodType.C => IsRoomC(p) && b.Pods.Where(x => IsRoomC(x.Key) && x.Key.Y > p.Y).All(x => x.Value == PodType.C),
+    PodType.D => IsRoomD(p) && b.Pods.Where(x => IsRoomD(x.Key) && x.Key.Y > p.Y).All(x => x.Value == PodType.D),
     _ => throw new ArgumentOutOfRangeException(nameof(type), type, null)
   };
 }
 
-bool IsCorrectRoom(Point p, AmphiPodType type) {
+bool IsCorrectRoom(Point p, PodType type) {
   return type switch {
-    AmphiPodType.A => IsRoomA(p),
-    AmphiPodType.B => IsRoomB(p),
-    AmphiPodType.C => IsRoomC(p),
-    AmphiPodType.D => IsRoomD(p),
+    PodType.A => IsRoomA(p),
+    PodType.B => IsRoomB(p),
+    PodType.C => IsRoomC(p),
+    PodType.D => IsRoomD(p),
     _ => throw new ArgumentOutOfRangeException(nameof(type), type, null)
   };
 }
@@ -205,44 +175,39 @@ bool IsRoomD(Point p) {
 }
 
 internal class Board {
-  public readonly List<Point> Points;
-  public IDictionary<Point, AmphiPodType> Amphipods;
-  public readonly int RoomSize;
-  
-  public IEnumerable<Point> AvailablePoints() {
-    return Points.Except(Amphipods.Keys).ToList();
-  }
+  public readonly IDictionary<Point, PodType> Pods;
+  private readonly int _roomSize;
 
   public bool IsFinished() {
-    if (Amphipods.Keys.Any(p => p.Y == 0)) return false;
-    return Amphipods.All(p => {
+    if (Pods.Keys.Any(p => p.Y == 0)) return false;
+    return Pods.All(p => {
       return p.Value switch {
-        AmphiPodType.A => p.Key.X == 2,
-        AmphiPodType.B => p.Key.X == 4,
-        AmphiPodType.C => p.Key.X == 6,
-        AmphiPodType.D => p.Key.X == 8,
+        PodType.A => p.Key.X == 2,
+        PodType.B => p.Key.X == 4,
+        PodType.C => p.Key.X == 6,
+        PodType.D => p.Key.X == 8,
         _ => throw new ArgumentOutOfRangeException()
       };
     });
   }
 
-  public bool IsRoomAvailable(AmphiPodType type) {
+  public bool IsRoomAvailable(PodType type) {
     return type switch {
-      AmphiPodType.A => Amphipods.Where(x => x.Key.X == 2).All(p => p.Value == AmphiPodType.A) &&
-                        Amphipods.Count(x => x.Key.X == 2) < RoomSize,
-      AmphiPodType.B => Amphipods.Where(x => x.Key.X == 4).All(p => p.Value == AmphiPodType.B) &&
-                        Amphipods.Count(x => x.Key.X == 4) < RoomSize,
-      AmphiPodType.C => Amphipods.Where(x => x.Key.X == 6).All(p => p.Value == AmphiPodType.C) &&
-                        Amphipods.Count(x => x.Key.X == 6) < RoomSize,
-      AmphiPodType.D => Amphipods.Where(x => x.Key.X == 8).All(p => p.Value == AmphiPodType.D) &&
-                        Amphipods.Count(x => x.Key.X == 8) < RoomSize,
+      PodType.A => Pods.Where(x => x.Key.X == 2)
+        .All(p => p.Value == PodType.A) && Pods.Count(x => x.Key.X == 2) < _roomSize,
+      PodType.B => Pods.Where(x => x.Key.X == 4)
+        .All(p => p.Value == PodType.B) && Pods.Count(x => x.Key.X == 4) < _roomSize,
+      PodType.C => Pods.Where(x => x.Key.X == 6)
+        .All(p => p.Value == PodType.C) && Pods.Count(x => x.Key.X == 6) < _roomSize,
+      PodType.D => Pods.Where(x => x.Key.X == 8)
+        .All(p => p.Value == PodType.D) && Pods.Count(x => x.Key.X == 8) < _roomSize,
       _ => throw new ArgumentOutOfRangeException(nameof(type), type, null)
     };
   }
 
   public (Board, int) Move(Point pod, Point newPos) {
-    var dict = new Dictionary<Point, AmphiPodType>();
-    foreach (var (key, value) in Amphipods) {
+    var dict = new Dictionary<Point, PodType>();
+    foreach (var (key, value) in Pods) {
       dict.Add(key, value);
     }
 
@@ -250,36 +215,30 @@ internal class Board {
     var costs = CalcCosts(pod, podType, newPos);
     dict.Remove(pod);
     dict.Add(newPos, podType);
-    return (new Board(dict, RoomSize), costs);
+    return (new Board(dict, _roomSize), costs);
+  }
+
+  private static int CalcCosts(Point pod, PodType podType, Point newPos) {
+    var length = pod.Y + Math.Abs(newPos.X - pod.X) + newPos.Y;
+    return length * (int)podType;
   }
 
   public override string ToString() {
-    return string.Join(";", Amphipods.OrderBy(x => x.Key.Y).ThenBy(x => x.Key.X)
-      .Select(x => $"{x.Value}_{x.Key.X}_{x.Key.Y}"));
+    return string.Join(",", Pods
+      .OrderBy(x => x.Key.Y)
+      .ThenBy(x => x.Key.X)
+      .Select(x => x.Key.Y + "_" + x.Key.X + "_" + x.Value));
   }
 
-  private static int CalcCosts(Point pod, AmphiPodType podType, Point newPos) {
-    var length = pod.Y + Math.Abs(newPos.X - pod.X) + newPos.Y;
-    return podType switch {
-      AmphiPodType.A => length,
-      AmphiPodType.B => length * 10,
-      AmphiPodType.C => length * 100,
-      AmphiPodType.D => length * 1000,
-      _ => throw new ArgumentOutOfRangeException(nameof(podType), podType, null)
-    };
-  }
-
-  public Board(Dictionary<Point, AmphiPodType> pods, int roomSize) {
-    RoomSize = roomSize;
-    Points = Enumerable.Range(0, 11).Select(x => new Point(x, 0)).ToList();
-    Points.AddRange(Enumerable.Range(1, 4).SelectMany(x => Enumerable.Range(1, RoomSize).Select(index => new Point(2 * x, index))));
-    Amphipods = pods;
+  public Board(IDictionary<Point, PodType> pods, int roomSize) {
+    _roomSize = roomSize;
+    Pods = pods;
   }
 }
 
-enum AmphiPodType {
-  A,
-  B,
-  C,
-  D
+internal enum PodType {
+  A = 1,
+  B = 10,
+  C = 100,
+  D = 1000
 }
